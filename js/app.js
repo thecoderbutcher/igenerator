3c5ea3e43ee4fe76bfa357fdac6e07216d4f2bd7
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2024-01-16 22:04:54 -0300

    feat: download btn function

--- js/app.js
+++ js/app.js
@@ -1,29 +1,59 @@
 const generateForm = document.forms['generate-form']
 const imgContainer = document.querySelector('.image-gallery')
-const OPENAI_API_KEY = '';
+const OPENAI_API_KEY = 'sk-dw3iS1FIeV4L0mhNRqyvT3BlbkFJZdTuHEA8DElKnxSB0';
+let isLoading = false;
+let HGB = '';
+
+const updateImageCard = (imgDataArray) => {
+    imgDataArray.forEach((imgObject, index) => {
+        const imgCard = imgContainer.querySelectorAll('.img-card')[index];
+        const imgElement = imgCard.querySelector('img');
+        const downloadBtn = imgCard.querySelector('.download-btn')
+
+        const aiGenerateImg = `data:image/jpeg;base64,${imgObject.b64_json}`;
+        imgElement.src = aiGenerateImg;
+
+        imgElement.onload = () => {
+            imgCard.classList.remove('loading');
+            downloadBtn.setAttribute('href', aiGenerateImg);
+            downloadBtn.setAttribute('download', `${new Date().getTime()}.jpg`)
+        }
+    });
+}
 
 const generateImages = async (text, quantity) => {
     try {
-        const response = await fetch('https://api.openai.com/v1/images/generations', {
+        const response = await fetch("https://api.openai.com/v1/images/generations", {
             method:'POST',
             headers:{
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${OPENAI_API_KEY}`,
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${OPENAI_API_KEY}`,
             },
             body: JSON.stringify({
                 prompt: text,
-                n: quantity,
+                n: parseInt(quantity),
                 size: '512x512',
                 response_format: 'b64_json'
             })
-        })
+        });
+
+        if(!response.ok) throw new Error('Failed to generate images! Please try again.');
+
+        const { data } = await response.json();
+        updateImageCard([...data]);
     } catch (error) {
-        console.log(error)
+        alert(error.message)
+    } finally{
+        isLoading = false;
     }
 }
 
 generateForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    if(isLoading) return;
+    isLoading = true;
+
     textPromp = generateForm['promt-input'].value;
     quantity = generateForm['img-quantity'].value
 
